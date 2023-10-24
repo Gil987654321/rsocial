308d805f07dfb11bc2d94183dd6a322bf324ec04
José-Gilberto <92352192+Gil987654321@users.noreply.github.com>
2023-10-24 17:56:39 -0600

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -2,10 +2,20 @@
 // Establish Connection to Database
 function connect() {
     static $conn;
-    if ($conn === NULL){ 
-        $conn = mysqli_connect('localhost','root','','socialnetwork');
+    if ($conn === NULL) {
+        $username = 'doadmin';
+        $password = 'AVNS_T_3d_BTX-xXyHK4xuRQ';
+        $host = 'db-mysql-nyc3-87335-do-user-14016281-0.c.db.ondigitalocean.com';
+        $port = 25060;
+        $database = 'defaultdb';
+        $sslmode = 'REQUIRED';
+
+        $conn = mysqli_connect($host, $username, $password, $database, $port);
+        
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
     }
     return $conn;
 }
-
 ?>

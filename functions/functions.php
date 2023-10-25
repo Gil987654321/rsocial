dfbcda05f8fc3066fb1d00c7ef74a428e1c3f2c5
José-Gilberto <92352192+Gil987654321@users.noreply.github.com>
2023-10-24 19:16:04 -0600

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -7,7 +7,7 @@ function connect() {
         $password = 'AVNS_T_3d_BTX-xXyHK4xuRQ';
         $host = 'db-mysql-nyc3-87335-do-user-14016281-0.c.db.ondigitalocean.com';
         $port = 25060;
-        $database = 'defaultdb';
+        $database = 'socialnetwork';
         $sslmode = 'REQUIRED';
 
         $conn = mysqli_connect($host, $username, $password, $database, $port);
